811aad41f47048e277de6f611a17da306b732d69
Kieran <>
2023-01-22 20:49:04 +0000

    Connected to rust graphql server using urql client.

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,40 @@
 import { createSignal } from "solid-js";
+import { Show } from "solid-js";
+import { createMutation, createQuery } from "solid-urql";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
+
+  const loginMutation = `
+  mutation {
+  login(input:{ password:"password", email:"kieran@email.com"})  
+  }`;
+
+  const deckQuery = `
+  query {
+  getDecks {
+    id
+  }
+}`;
+
+  const [result, login] = createMutation(loginMutation);
+
+  const [decks, deckState] = createQuery({
+    query: deckQuery,
+  });
+
+  login();
+
+  console.log("hello", result());
+
   return (
-    <button
-      class="w-[200px] rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-[2rem] py-[1rem]"
-      onClick={() => setCount(count() + 1)}
-    >
-      Clicks: {count()}
-    </button>
+    <Show when={!deckState().fetching} fallback={<p>Loading...</p>}>
+      <p>
+        Items are:{" "}
+        {decks()
+          .getDecks.map((item: any) => item.title)
+          .join()}
+      </p>
+    </Show>
   );
 }
--- src/root.tsx
+++ src/root.tsx
@@ -14,6 +14,11 @@ import {
   Title,
 } from "solid-start";
 import "./root.css";
+import { Provider, createClient } from "solid-urql";
+
+const client = createClient({
+  url: "http://0.0.0.0:8080",
+});
 
 export default function Root() {
   const location = useLocation();
@@ -24,30 +29,32 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>Flashed</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <nav class="bg-sky-800">
-              <ul class="container flex items-center p-3 text-gray-200">
-                <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-                  <A href="/">Home</A>
-                </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-                  <A href="/about">About</A>
-                </li>
-              </ul>
-            </nav>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
-      </Body>
+      <Provider value={client}>
+        <Body>
+          <Suspense>
+            <ErrorBoundary>
+              <nav class="bg-sky-800">
+                <ul class="container flex items-center p-3 text-gray-200">
+                  <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+                    <A href="/">Home</A>
+                  </li>
+                  <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
+                    <A href="/about">About</A>
+                  </li>
+                </ul>
+              </nav>
+              <Routes>
+                <FileRoutes />
+              </Routes>
+            </ErrorBoundary>
+          </Suspense>
+          <Scripts />
+        </Body>
+      </Provider>
     </Html>
   );
 }
